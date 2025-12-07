25037097c65d1debbd72a6ce2d4ea2bb5a7fcea0
Generic Owl <242839885+GenericImaginaryOwl@users.noreply.github.com>
2025-12-07 20:23:45 +0400

    Add function that chooses an operation to perform

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
+let operandA = null;
+let operandB = null;
+let operator = null;
+
 function add(a, b) {
-    return a + b;
+    return +a + +b;
 }
 
 function subtract(a, b) {
@@ -12,8 +16,31 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b !== 0){
-        return a/b;
+        return Math.floor((a / b ) * 10 ** 10) / (10 ** 10);
     }else{
-        return "Cannot divide by zero";
+        return "Cannot divide by zero.";
+    }
+}
+
+function operate() {
+    let result = null;
+    switch (operator){
+        case "+":
+            result = add(operandA, operandB);
+            break;           
+        case "-":
+            result = subtract(operandA, operandB);
+            break;
+        case "*":
+            result = multiply(operandA, operandB);
+            break;
+        case "/":
+            result = divide(operandA, operandB);
+            break;
+    }
+    if (result !== null) {
+        console.log(result);
+    }else{
+        console.log("Something went wrong when performing the operation.");
     }
 }
